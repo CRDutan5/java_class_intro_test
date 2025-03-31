96796b39dea9c11a6386b3940211ad64dbfead5a
Carlitos <carlitosdutan@pursuit.org>
2025-03-31 15:39:56 -0400

    finished assignment

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,9 +1,78 @@
 package org.example;
 
 public class Car {
+    private String model;
+    private String make;
+    private int year;
+    private double price;
+
+    public Car(String model, String make, int year, double price) {
+        this.model = model;
+        this.make = make;
+        this.year = year;
+        this.price = price;
+    }
+
+    public Car() {
+        this.model = "Unknown";
+        this.make = "Unknown";
+        this.year = 2000;
+        this.price = 0.0;
+    }
 
 
 
+    public double getPrice() {
+        return price;
+    }
 
+    public void setPrice(double price) {
+        this.price = Math.max(price, 0.0);
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        if (year <= 1886) {
+            this.year = 1886;
+        }
+        else if (year >= 2024) {
+            this.year = 2024;
+        }
+        else {
+            this.year = year;
+        }
+    }
+
+    public double calculateDepreciation() {
+        if (this.year == 2025) {
+            return this.price;
+        }
+        else {
+            return this.price * Math.pow(0.85, 5);
+        }
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
 
 }
